3822a0e23ce5e2a1d6cf22dc87889dfa6021aed8
ljonesdesign <larry@ljonesdesign.com>
2023-10-20 11:54:17 -0400

    Add files via upload

---
+++ 01_select_file_enter_term.py
@@ -0,0 +1,53 @@
+#--------------------------------------------------
+# DO NOT UNCOMMENT COMMENTS WITHIN DASHED LINES!
+#--------------------------------------------------
+
+#--------------------------------------------------
+# Make program repeat:
+#--------------------------------------------------
+
+# while True
+
+#--------------------------------------------------
+# get user input for file:
+#--------------------------------------------------
+
+# file_variable = input(your input)
+
+#--------------------------------------------------
+# Sample output for file_variable statement:
+#--------------------------------------------------
+
+# i  a) read 80s-music.txt
+# if b) read athletes.txt
+# if x) exit
+
+#--------------------------------------------------
+# process user input 
+#--------------------------------------------------
+
+# if file_variable is x 
+#    break
+# elif file_variable a
+#     file_variable is 80s-music.txt
+# elif b
+#     file_variable is athletes.txt
+# else
+#     Invalid option. Please select a, b, or x.
+#     continue
+
+#--------------------------------------------------
+# enter a search term 
+#--------------------------------------------------
+
+# search_variable = input(your input)
+
+#--------------------------------------------------
+# Sample output for above:
+#--------------------------------------------------
+
+# Enter your search term for {file_variable} file.
+
+#--------------------------------------------------
+# go to 02_search_for_term.py to continue .....
+#--------------------------------------------------
---
+++ 02_search_for_term.py
@@ -0,0 +1,69 @@
+#--------------------------------------------------
+# See if the term is in the chosen file.
+# Do this by creating parameters from the variables 
+# you created
+#--------------------------------------------------
+
+# def your_function(file_variable,search_variable):
+
+#--------------------------------------------------
+# open and read the file
+#--------------------------------------------------
+
+#     with open file_variable read as file
+#          content = file.read()  
+
+#--------------------------------------------------
+# now the file is in the memory buffer as content
+#
+# Next check to see if the search_variable is in the
+# content buffer:
+#--------------------------------------------------
+
+#          if search_variable in content
+
+#--------------------------------------------------
+# if it is print that it is there and ask if user
+# wants to see the entries for the term
+#--------------------------------------------------
+              
+#              print Your search term {search_variable} exists in the {file_variable} file!
+#              Would you like to see the entries? (y or n)?
+
+               #-----------------------------------------------------
+               # if y then run this code to output all the entries:
+               #-----------------------------------------------------
+
+#              if Y lowercase is y
+#                  print this: Here are all of the entries with the term {search variable}:
+#                  with open file variable read as file:
+#                      for line in file
+#                          if search_variable in line
+#                              print the line with the white_space stripped
+
+               #-----------------------------------------------------
+               # if n then run this code:
+               #-----------------------------------------------------
+
+#              elif Y lowercase is n
+#                  print Goodbye
+
+               #-----------------------------------------------------
+               # if y then run this code to output all the entries:
+               #-----------------------------------------------------
+
+#              else
+#                  print Invalid option. Please enter y or n.
+
+#--------------------------------------------------
+# if it is not, print that it is there
+#--------------------------------------------------
+#          else
+#              search_variable does not exist in file_variable
+
+#--------------------------------------------------
+# call the function 
+#--------------------------------------------------
+
+# your_function(file_variable,search variable)   
+               
